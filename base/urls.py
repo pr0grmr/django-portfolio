772c3d462abe5782f7792dd03bc0e785165e2795
Harkaran Singh <sinhy040@mymail.unisa.edu.au>
2022-07-20 20:43:26 +0930

    Added Projects

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("projects/<str:pk>", views.projects, name="projects"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
